f5fa1912287e3e077c936044ebfd09618674d132
Isajloski <goxsy1@gmail.com>
2024-07-06 19:12:36 +0200

    Answers: -> Image MouseHover added -> Image MouseAdded Leave added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,10 +33,11 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.labelQuestion = new System.Windows.Forms.Label();
             this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
             this.labelB = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.labelC = new System.Windows.Forms.Label();
+            this.labelD = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // imageList1
@@ -51,13 +52,30 @@
             this.labelQuestion.BackColor = System.Drawing.Color.Transparent;
             this.labelQuestion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.labelQuestion.Name = "labelQuestion";
-            this.labelQuestion.Click += new System.EventHandler(this.label1_Click);
             // 
             // imageList2
             // 
             this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
             resources.ApplyResources(this.imageList2, "imageList2");
             this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.UseCompatibleTextRendering = true;
+            // 
+            // labelA
+            // 
+            resources.ApplyResources(this.labelA, "labelA");
+            this.labelA.BackColor = System.Drawing.Color.Transparent;
+            this.labelA.ForeColor = System.Drawing.Color.Transparent;
+            this.labelA.Name = "labelA";
+            this.labelA.UseCompatibleTextRendering = true;
+            this.labelA.MouseLeave += new System.EventHandler(this.labelA_MouseLeave);
+            this.labelA.MouseHover += new System.EventHandler(this.labelA_MouseHover);
             // 
             // labelB
             // 
@@ -66,40 +84,39 @@
             this.labelB.ForeColor = System.Drawing.Color.Transparent;
             this.labelB.Name = "labelB";
             this.labelB.UseCompatibleTextRendering = true;
+            this.labelB.MouseLeave += new System.EventHandler(this.labelB_MouseLeave);
+            this.labelB.MouseHover += new System.EventHandler(this.labelB_MouseHover);
             // 
-            // label1
+            // labelC
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Name = "label1";
-            this.label1.UseCompatibleTextRendering = true;
+            resources.ApplyResources(this.labelC, "labelC");
+            this.labelC.BackColor = System.Drawing.Color.Transparent;
+            this.labelC.ForeColor = System.Drawing.Color.Transparent;
+            this.labelC.Name = "labelC";
+            this.labelC.UseCompatibleTextRendering = true;
+            this.labelC.MouseLeave += new System.EventHandler(this.labelC_MouseLeave);
+            this.labelC.MouseHover += new System.EventHandler(this.labelC_MouseHover);
             // 
-            // label2
+            // labelD
             // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.ForeColor = System.Drawing.Color.Transparent;
-            this.label2.Name = "label2";
-            this.label2.UseCompatibleTextRendering = true;
-            // 
-            // label3
-            // 
-            resources.ApplyResources(this.label3, "label3");
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.ForeColor = System.Drawing.Color.Transparent;
-            this.label3.Name = "label3";
-            this.label3.UseCompatibleTextRendering = true;
+            resources.ApplyResources(this.labelD, "labelD");
+            this.labelD.BackColor = System.Drawing.Color.Transparent;
+            this.labelD.ForeColor = System.Drawing.Color.Transparent;
+            this.labelD.Name = "labelD";
+            this.labelD.UseCompatibleTextRendering = true;
+            this.labelD.MouseLeave += new System.EventHandler(this.labelD_MouseLeave);
+            this.labelD.MouseHover += new System.EventHandler(this.labelD_MouseHover);
             // 
             // Милионер
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.labelD);
+            this.Controls.Add(this.labelC);
             this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelQuestion);
             this.Name = "Милионер";
             this.Load += new System.EventHandler(this.Милионер_Load);
             this.ResumeLayout(false);
@@ -110,10 +127,11 @@
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Label labelQuestion;
         private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelA;
         private System.Windows.Forms.Label labelB;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.Label labelD;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,45 +13,84 @@ namespace Милионер
 {
     public partial class Милионер : Form
     {
+
+        // Hover over an answer image
+        public Image hoverAnswer {  get; set; }
+        // Correct answer image
+        public Image correctAnswer { get; set; }
+        // Incorrect answer image
+        public Image incorrectAnswer { get; set; }
+        public Level level { get; set; }
         public Милионер()
         {
             InitializeComponent();
+            level = new Level();
+            hoverAnswer = imgAnswer.answer_hover;
+            correctAnswer = imgAnswer.correct;
+            incorrectAnswer = imgAnswer.incorrect;
         }
-
-        private void ScreenSaverForm_Load(object sender, EventArgs e)
-        {
-          
-
-        }
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_MouseHover(object sender, EventArgs e)
-        {
-            Console.WriteLine("Hovered!");
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void Милионер_Load(object sender, EventArgs e)
         {
+        }
 
+        private void changeHoverImage(System.Windows.Forms.Label label)
+        {
+            label.Image = hoverAnswer;
+        }
+
+        private void changeLeaveImage(System.Windows.Forms.Label label)
+        {
+            label.Image = null;
+        }
+
+        private void labelA_MouseHover(object sender, EventArgs e)
+        {
+            changeHoverImage(labelA);
+        }
+
+  
+        private void labelA_MouseLeave(object sender, EventArgs e)
+        {
+            changeLeaveImage(labelA);
+            Console.WriteLine("A: Leave");
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void labelB_MouseHover(object sender, EventArgs e)
         {
+            changeHoverImage(labelB);
+        }
 
+        private void labelB_MouseLeave(object sender, EventArgs e)
+        {
+            Console.WriteLine("B: Leave");
+            changeLeaveImage(labelB);
+
+        }
+
+        private void labelC_MouseHover(object sender, EventArgs e)
+        {
+            changeHoverImage(labelC);
+
+        }
+
+        private void labelC_MouseLeave(object sender, EventArgs e)
+        {
+            Console.WriteLine("C: Leave");
+            changeLeaveImage(labelC);
+
+        }
+
+        private void labelD_MouseHover(object sender, EventArgs e)
+        {
+            changeHoverImage(labelD);
+
+        }
+
+        private void labelD_MouseLeave(object sender, EventArgs e)
+        {
+            Console.WriteLine("D: Leave");
+            changeLeaveImage(labelD);
         }
     }
 }
